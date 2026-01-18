7b443183db227393f048a724d31385da8b7d7c0b
Zaryif Azfar <zaryif@Zaryifs-MacBook-Pro.local>
2026-01-18 06:21:25 +0600

    Fix: Increase static page generation timeout to 300s

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/devcafe-bangladesh',
-  assetPrefix: '/devcafe-bangladesh/',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+    output: 'export',
+    basePath: '/devcafe-bangladesh',
+    assetPrefix: '/devcafe-bangladesh/',
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
+    staticPageGenerationTimeout: 300,
 }
 
 module.exports = nextConfig
